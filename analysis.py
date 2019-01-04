6db92972dd20f91cc8187c2384d80768a25a324c
ArushiSinghal <arushisinghal19971997@gmail.com>
2019-01-04 23:19:55 +0530

    changes

--- analysis.py
+++ analysis.py
@@ -8,14 +8,9 @@ from os.path import isfile, join
 import pandas as pd
 import numpy as np
 
-hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none', 'Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'}
-
 data = pd.read_csv("a5.csv", sep=",")
 df = data.values
-a = df[0][0]
-print (a)
-print (type(a))
-#data = data.iloc[2:]
+df = np.insert(df, np.size(df,1), 0, axis=1)
+for i in range(np.size(df,0)):
+    df[i][4] = df[i][1] + df[i][2]
 print (df)
-print (type(df[0][0]))
-print(data.head())
